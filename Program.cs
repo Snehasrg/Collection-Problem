3e2c0f2f5c098f73fd565419f99e8b2480f917a0
Snehasrg <sghongade642@gmail.com>
2023-04-05 22:02:14 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CollectionPracticeProblem;
+namespace CollectionPracticeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chapter chaptercount = new Chapter();
+            //chaptercount.RemainingChapters();
+
+            MaxSellPrice maxSellPrice = new MaxSellPrice();
+            maxSellPrice.MaxSell();
+        }
+    }
+}
