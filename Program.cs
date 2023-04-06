28ecaa4abd15b84dc38808815ca2acb759e9d7c1
Snehasrg <sghongade642@gmail.com>
2023-04-06 22:52:13 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -6,11 +6,19 @@ namespace CollectionPracticeProblem
     {
         static void Main(string[] args)
         {
-            //Chapter chaptercount = new Chapter();
-            //chaptercount.RemainingChapters();
+            Console.WriteLine("Select any one option from following:-\n" +
+                                "1. Count Of Remaining Chapters");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Chapter chaptersCount = new Chapter();
+                    chaptersCount.RemainingChapters();
+                    break;
 
-            MaxSellPrice maxSellPrice = new MaxSellPrice();
-            maxSellPrice.MaxSell();
+                default:
+                    break;
+            }
         }
     }
 }
