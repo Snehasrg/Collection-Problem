0a0cb6b3f21324eef13fadededfafe2874ebd25e
Snehasrg <sghongade642@gmail.com>
2023-06-29 20:02:41 +0530

    [sneha]Added program for max selling price

--- Program.cs
+++ Program.cs
@@ -7,13 +7,17 @@ namespace CollectionPracticeProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Select any one option from following:-\n" +
-                                "1. Count Of Remaining Chapters");
+                                "1. Count Of Remaining Chapters.\n2. Max Selling Price.");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     Chapter chaptersCount = new Chapter();
                     chaptersCount.RemainingChapters();
+                    break;
+                case 2:
+                    MaxSellPrice maxSellPrice = new MaxSellPrice();
+                    maxSellPrice.MaxSell();
                     break;
 
                 default:
